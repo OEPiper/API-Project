c700308837392be7cd7573d5c9ec84cbdcfa3124
Matt Piper <oepiper@gmail.com>
2023-08-08 16:10:10 -0400

    fixed relation

--- backend/db/migrations/20230808151245-create-review-image.js
+++ backend/db/migrations/20230808151245-create-review-image.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Users',
+          model: 'Reviews',
           key: 'id'
         },
         onDelete: 'cascade'
