2dc5b49ccbfa39e6fdb5be54ac2c41fb0ce01ca1
Matt Piper <oepiper@gmail.com>
2023-08-09 16:29:47 -0400

    get-spots complete

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,32 +3,102 @@ const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { Spot, User } = require('../../db/models');
+const { Spot, User, Review, ReviewImage, SpotImage } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
-router.get('/current', requireAuth, async(res, req) => {
-    const { user } = req;
-    console.log(user)
-    if (user) {
-      const safeUser = {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        username: user.username,
-      };
-      return res.json({
-        user: safeUser
-      });
-    } else return res.json({ user: null });
+router.get('/current', requireAuth, async(req, res) => {
+    const userId = req.user.id;
+    const userSpots = await Spot.findAll({
+      where: {
+        ownerId: userId
+      },
+      include: [{
+        model:Review,
+        attributes: ['stars'],
+      },{
+        model: SpotImage 
+      }
+    ]
+  });
   
+  let spotsList = [];
+  userSpots.forEach((spot) =>{
+    spotsList.push(spot.toJSON())
+  })
+  spotsList.forEach((spot) => {
+    let starSum = 0
+    spot.Reviews.forEach((reviews) => {
+      starSum += reviews.stars
+    })
+    let starAvg = starSum/spot.Reviews.length;
+    spot.avgRating = starAvg
+    spot.previewImage = spot.SpotImages[0].url
+    delete spot.Reviews
+    
+    delete spot.SpotImages
+  })
+  res.json(spotsList)
+})
+  
+  
+router.get('/:spotId', async(req,res) => {
+    const spotId = req.params.spotId;
+    let spot = await Spot.findByPk(spotId, {
+      include: [{
+        model: Review
+      }, {model: SpotImage, attributes: ['id', 'url', 'preview']},
+    {model: User, attributes: ['id', 'firstName', 'lastName']}]
+    });
+    if(!spot){
+      res.status(404);
+      res.json({message: "Spot couldn't be found"})
+    }
+    //const images = await spot.getSpotImages({})
+    spot = spot.toJSON();
+    let starSum = 0
+      spot.Reviews.forEach((reviews) => {
+        starSum += reviews.stars
+      })
+      let starAvg = starSum/spot.Reviews.length;
+      spot.numReviews = spot.Reviews.length
+      spot.avgRating = starAvg
+      spot.Owner = spot.User
+      delete spot.Reviews
+      delete spot.User
+   
+    
+    res.json(spot)
+      
 })
 
 router.get('/', async(req,res) => {
-    const spots = await Spot.findAll();
-    res.json({Spots: [spots]});
+    const spots = await Spot.findAll({
+      include:[{
+        model: Review
+      }, {
+        model: SpotImage
+      }]
+    });
+
+    let spotsList = [];
+    spots.forEach((spot) =>{
+      spotsList.push(spot.toJSON())
+    })
+    spotsList.forEach((spot) => {
+      let starSum = 0
+      spot.Reviews.forEach((reviews) => {
+        starSum += reviews.stars
+      })
+      let starAvg = starSum/spot.Reviews.length;
+      spot.avgRating = starAvg
+      spot.previewImage = spot.SpotImages[0].url
+      delete spot.Reviews
+      
+      delete spot.SpotImages
+    })
+    res.json(spotsList)
 })
 
 module.exports = router;
