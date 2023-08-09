43d7620014d997698a72e6026a329a484252b593
Matt Piper <oepiper@gmail.com>
2023-08-09 19:17:58 -0400

    create-spot complete

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,6 +8,58 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+const validateSpot = [
+  check('address')
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check('city')
+    .exists({ checkFalsy: true })
+    .withMessage("City is required"),
+  check('state')
+    .exists({ checkFalsy: true })
+    .withMessage('State is required'),
+  check('country')
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check('lat')
+    .exists({ checkFalsy: true })
+    .withMessage("Latitude is not valid"),
+  check('lng')
+    .exists({ checkFalsy: true })
+    .withMessage("Longitude is not valid"),
+  check('name')
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50})
+    .withMessage("Name must be less than 50 characters"),
+  check('description')
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check('price')
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day is required"),
+  handleValidationErrors
+];
+
+router.post('/', requireAuth, validateSpot, async(req,res) => {
+  const {address, city, state, country, lat, lng, name, description, price} = req.body;
+  const ownerId = req.user.id;
+  
+  const newSpot = await Spot.create({
+    ownerId: ownerId,
+    address: address,
+    city: city,
+    state: state,
+    country: country,
+    lat: lat,
+    lng: lng,
+    name: name,
+    description: description,
+    price: price
+  });
+  res.json(newSpot)
+  
+})
+
 router.get('/current', requireAuth, async(req, res) => {
     const userId = req.user.id;
     const userSpots = await Spot.findAll({
