a4f4b525e933acecc63625f154d13f22a305067f
Matt Piper <oepiper@gmail.com>
2023-08-08 20:29:06 -0400

    get current spots started

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,14 +1,34 @@
-const express = require('express')
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { Spot, User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+router.get('/current', requireAuth, async(res, req) => {
+    const { user } = req;
+    console.log(user)
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  
+})
+
 router.get('/', async(req,res) => {
     const spots = await Spot.findAll();
-    res.json(spots);
+    res.json({Spots: [spots]});
 })
 
 module.exports = router;
