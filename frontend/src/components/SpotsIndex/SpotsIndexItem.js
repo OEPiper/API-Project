ea8493fec6cd151851810833bbf2f1627528b242
Matt Piper <oepiper@gmail.com>
2023-08-28 19:51:27 -0400

    hopefully fixed spot price

--- frontend/src/components/SpotsIndex/SpotsIndexItem.js
+++ frontend/src/components/SpotsIndex/SpotsIndexItem.js
@@ -10,13 +10,16 @@ const SpotIndexItem = ({spot}) => {
     if(Number.isInteger(spot.avgRating)){
         spot.avgRating = spot.avgRating.toFixed(1)
     }
+    if(Number.isInteger(spot.price)){
+        spot.price = spot.price.toFixed(2)
+    }
     
     return (
         <li>
             <img className='spot-img' src={spot.previewImage} />
             <p>{spot.city}, {spot.state}</p>
             <p>{spot.avgRating}</p>
-            <p>${spot.price.toFixed(2)} per night</p>
+            <p>${spot.price} per night</p>
         </li>
     )
 }
